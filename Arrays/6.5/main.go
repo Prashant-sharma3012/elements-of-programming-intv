7e70c6fc987aeb14e0d066b0e544ba42732f6c2c
prashant sharma <prashant.sharma@enquero.com>
2020-05-15 15:43:02 +0530

    remove duplicates from sorted array with o(n) time and 0(1) space

--- Arrays/6.5/main.go
+++ Arrays/6.5/main.go
@@ -1,7 +1,35 @@
 package main
 
+import "fmt"
+
 // remove duplicates from a sorted array in o(n) time and 0(1) space
 // [2,3,4,4,5,5,7,7,9,11] => [2,3,4,5,7,9,11,0,0,0]
 func main() {
+	input := []int{2, 3, 4, 4, 4, 4, 5, 5, 5, 5, 7, 7, 7, 7, 9, 11}
 
+	currentNum := input[0]
+	currentPos := 0
+
+	for indx := range input {
+		if indx == len(input)-1 {
+			break
+		}
+
+		if currentNum == input[indx+1] {
+			input[indx+1] = 0
+			if input[indx] != 0 {
+				currentPos = indx + 1
+			}
+		} else {
+			currentNum = input[indx+1]
+
+			if currentPos != 0 {
+				input[currentPos] = input[indx+1]
+				input[indx+1] = 0
+				currentPos++
+			}
+		}
+	}
+
+	fmt.Println(input)
 }
