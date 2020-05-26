f29f3d8e27687bfd4906abd4b7792b87b2e68bac
prashant sharma <prashant.sharma@enquero.com>
2020-05-26 15:43:19 +0530

    print all primes from 0 to input n

--- Arrays/6.8/main.go
+++ Arrays/6.8/main.go
@@ -1,4 +1,36 @@
 package main
 
+import "fmt"
+
 // list all prime numbers between 1 to input
-func main() {}
+func main() {
+	// input is n a number
+	n := 43
+	var inputArr []int
+	var isPrime []bool
+	var primes []int
+
+	for i := 0; i <= 43; i++ {
+		inputArr = append(inputArr, i)
+
+		if i == 0 || i == 1 {
+			isPrime = append(isPrime, false)
+		} else {
+			isPrime = append(isPrime, true)
+		}
+	}
+
+	for indx, val := range inputArr {
+		if isPrime[indx] {
+			primes = append(primes, val)
+
+			for j := indx + 1; j <= n; j++ {
+				if j%val == 0 && j < len(isPrime) {
+					isPrime[j] = false
+				}
+			}
+		}
+	}
+
+	fmt.Println(primes)
+}
