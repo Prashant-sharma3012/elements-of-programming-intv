dc14a0a123af40a9d835395708e77ae809da28e5
prashant sharma <prashant.sharma@enquero.com>
2020-05-29 00:24:20 +0530

    started to work but is runnign twice, looking into it

--- strings/7.7/main.go
+++ strings/7.7/main.go
@@ -12,7 +12,29 @@ import (
 var result []string
 var phoneEncoding = []string{"0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"}
 
+func alreadyProcessed(input []string) bool {
+	var cpy []string
+	found := false
+
+	cpy = append(cpy, input...)
+	elementTosearch := strings.Join(input, "")
+
+	for _, val := range cpy {
+		if val == elementTosearch {
+			found = true
+			return found
+		}
+	}
+
+	return found
+}
+
 func getCombinations(input []string) {
+
+	if alreadyProcessed(input) {
+		return
+	}
+
 	var temp []string
 	// if all strings are of length 1
 	// [A, G, M] merge and push to result
@@ -27,19 +49,22 @@ func getCombinations(input []string) {
 		return
 	}
 
+	var inputCpy1, inputCpy2 []string
+	inputCpy1 = append(inputCpy1, input...)
+	inputCpy2 = append(inputCpy2, input...)
+
 	for indx, val := range input {
 		if len(val) > 1 {
 			var path1, path2, temp1, temp2 []string
-
 			// split and call again
 			parts := strings.SplitN(val, "", 2)
-			temp1 = append([]string{parts[0]}, input[indx+1:]...)
-			path1 = append(input[:indx], temp1...)
+			temp1 = append([]string{parts[0]}, inputCpy1[indx+1:]...)
+			path1 = append(inputCpy1[:indx], temp1...)
 
 			getCombinations(path1)
 
-			temp2 = append([]string{parts[1]}, input[indx+1:]...)
-			path2 = append(input[:indx], temp2...)
+			temp2 = append([]string{parts[1]}, inputCpy2[indx+1:]...)
+			path2 = append(inputCpy2[:indx], temp2...)
 			getCombinations(path2)
 		}
 	}
