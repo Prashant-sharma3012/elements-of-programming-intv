f4c14b74666849970ce5b0580cc4bc01cd90c6fb
prashant sharma <prashant.sharma@enquero.com>
2020-07-01 19:27:00 +0530

    implenting binary search trees

---
+++ binaryTrees/bstree/main.go
@@ -0,0 +1,139 @@
+package bstree
+
+type BSTreeInt struct {
+	val   int
+	left  *BSTreeInt
+	right *BSTreeInt
+}
+
+func GetBTreeInt(val int) *BSTreeInt {
+	return &BSTreeInt{
+		val:   val,
+		left:  nil,
+		right: nil,
+	}
+}
+
+func addToTree(val int, b *BSTreeInt) {
+	if b.val > val {
+		if b.left == nil {
+			b.left = &BSTreeInt{
+				val:   val,
+				left:  nil,
+				right: nil,
+			}
+			return
+		}
+		addToTree(val, b.left)
+		return
+	} else {
+		if b.right == nil {
+			b.right = &BSTreeInt{
+				val:   val,
+				left:  nil,
+				right: nil,
+			}
+			return
+		}
+
+		addToTree(val, b.right)
+		return
+	}
+}
+
+func find(val int, b *BSTreeInt) bool {
+	if b.val == val {
+		return true
+	} else {
+		if b.val > val {
+			return find(val, b.left)
+		}
+		return find(val, b.right)
+	}
+
+	return false
+}
+
+func removeFromTree(val int, b *BSTreeInt) {
+
+}
+
+func (b *BSTreeInt) Insert(val int) {
+	addToTree(val, b)
+}
+
+func (b *BSTreeInt) Has(val int) bool {
+	return find(val, b)
+}
+
+func (b *BSTreeInt) Remove(val int) {
+
+}
+
+var inOrder []int
+
+func traverseInOrder(b *BSTreeInt) []int {
+	if b.left != nil {
+		traverseInOrder(b.left)
+	}
+
+	inOrder = append(inOrder, b.val)
+
+	if b.right != nil {
+		traverseInOrder(b.right)
+	}
+
+	return inOrder
+}
+
+// Left Root Right
+func (b *BSTreeInt) InOrder() []int {
+	return traverseInOrder(b)
+}
+
+var preOrder []int
+
+func traversePreOrder(b *BSTreeInt) []int {
+
+	preOrder = append(preOrder, b.val)
+
+	if b.left != nil {
+		traverseInOrder(b.left)
+	}
+
+	if b.right != nil {
+		traverseInOrder(b.right)
+	}
+
+	return inOrder
+}
+
+// Root Left Right
+func (b *BSTreeInt) PreOrder() []int {
+	return traversePreOrder(b)
+}
+
+var postOrder []int
+
+func traversePostOrder(b *BSTreeInt) []int {
+	if b.left != nil {
+		traverseInOrder(b.left)
+	}
+
+	if b.right != nil {
+		traverseInOrder(b.right)
+	}
+
+	postOrder = append(postOrder, b.val)
+
+	return inOrder
+}
+
+// Left Right Root
+func (b *BSTreeInt) PostOrder() []int {
+	return traversePostOrder(b)
+}
+
+func (b *BSTreeInt) LevelOrder() {
+
+}
